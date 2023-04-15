11f497575d3b9ee57d36e7e2b057327322dde890
Basavaraj-PN <bnarasapur@gmail.com>
2023-04-15 17:33:17 +0530

    adapting main code to parameter server

--- orb3_ros2_wrapper/src/mono_node.cpp
+++ orb3_ros2_wrapper/src/mono_node.cpp
@@ -66,8 +66,8 @@ int main(int argc, char **argv)
   readParametersFromParameterServer();
 
   ORB_SLAM3::System::eSensor sensor_type = ORB_SLAM3::System::MONOCULAR;
-  ORB_SLAM3::System SLAM("src/orb3_wrapper/config/ORBvoc.txt", "src/orb3_wrapper/config/realsense_d435i.yaml", sensor_type, true);
-  rclcpp::spin(std::make_shared<Monocular>("monocular_node", "/carla/ego_vehicle/rgb_front/image", Sophus::SE3f(), &SLAM));
+  ORB_SLAM3::System SLAM(ORBvoc_file, realsense_d435i_file, sensor_type, enable_pangolin);
+  rclcpp::spin(std::make_shared<Monocular>("monocular_node", image_topic, Sophus::SE3f(), &SLAM));
   rclcpp::shutdown();
   return 0;
 }
